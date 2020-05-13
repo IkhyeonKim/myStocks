09f28d535bf77ed0c16890bb32e8aad410170f87
Ikhyeon <kih199212@gmail.com>
2020-05-14 01:26:54 +0900

    Add DB connection

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,7 @@
 import path from 'path'
 import express from 'express'
+import mysql from 'mysql'
+import * as config from './config.example.json'
 
 const app = express();
 const DIST_DIR = __dirname;
@@ -14,3 +16,23 @@ const PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
     console.log(`App listening to ${PORT}....`)
 })
+
+const connection = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database,
+    port: config.port
+})
+
+connection.connect()
+
+connection.query('SELECT * FROM tasks', function(error, results, fields) {
+    if(error){
+        throw error;    
+    }
+
+    console.log(results)
+})
+
+connection.end
